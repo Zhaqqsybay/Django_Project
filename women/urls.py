469e823ef345b7a2c03e10ef0624fbad0467610b
Zhaqqsybay <sayat.zhaksybay@narxoz.kz>
2023-02-25 21:38:17 +0600

    Django_Project

--- women/urls.py
+++ women/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('addpage/', contact, name='contact'),
     path('addpage/', login, name='login'),
     path('post/<int:post_id>/', show_post, name='post'),
+    path('category/<int:cat_id>/', show_category, name='category'),
 ]
