0cece2d24cfc808ebfa1d6a41ce48f498e4b1ccd
Zhaqqsybay <sayat.zhaksybay@narxoz.kz>
2023-04-09 21:12:10 +0600

    Lab 9

--- women/admin.py
+++ women/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import *
 
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display = ('id', 'title', 'time_create', 'get_html_photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {"slug": ("title",)}
+    fields = ('title', 'slug', 'cat', 'content', 'photo', 'get_html_photo', 'is_published', 'time_create', 'time_update')
+    readonly_fields = ('time_create', 'time_update', 'get_html_photo')
+    save_on_top = True
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width=50>")
+
+    get_html_photo.short_description = "Миниатюра"
 
 
 class CategoryAdmin(admin.ModelAdmin):
--- women/utils.py
+++ women/utils.py
@@ -8,7 +8,7 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 ]
 
 class DataMixin:
-    paginate_by = 20
+    paginate_by = 3
 
     def get_user_context(self, **kwargs):
         context = kwargs
