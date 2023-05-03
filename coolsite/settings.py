c56ec65bc7e58fbb9f927d1d5ce60deb9ecb43df
Zhaqqsybay <sayat.zhaksybay@narxoz.kz>
2023-05-03 15:44:56 +0600

    Hosting

--- coolsite/settings.py
+++ coolsite/settings.py
@@ -83,11 +83,17 @@ WSGI_APPLICATION = 'coolsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'ozjl5ehIxXEpcO34rj7r',
+        'HOST': 'containers-us-west-135.railway.app',
+        'PORT': '6876',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
